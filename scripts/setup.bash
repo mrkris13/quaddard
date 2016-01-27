073e7288257e17e45958f6037cf8120cc631a953
Kris Frey <kfrey@mit.edu>
2016-01-26 21:54:48 -0500

    Added scripts for setup

---
+++ scripts/setup.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# source ROS workspace
+source /opt/ros/indigo/setup.bash
+source $HOME/quaddard/devel/setup.bash
+
+# define aliases
+alias cdfsw='cd $HOME/quaddard/fsw'
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# append to bashrc
+echo 'source $HOME/quaddard/scripts/setup.bash' >> $HOME/.bashrc
