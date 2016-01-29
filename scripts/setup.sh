293ef9f60617cdab1b68758578b556eb34122e0a
Kris Frey <kfrey@mit.edu>
2016-01-28 19:52:09 -0500

    Added network config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,7 @@
 
 # append to bashrc
 echo 'source $HOME/quaddard/scripts/setup.bash' >> $HOME/.bashrc
+
+# set up networking
+sudo cp interfaces /etc/network/interfaces
+sudo cp wpa_supplicant.conf /etc/wpa_supplicant.conf
