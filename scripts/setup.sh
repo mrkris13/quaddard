68162ccd634dd83be55279f45137363de4e6fdae
Kris Frey <kfrey@mit.edu>
2016-02-01 14:05:37 -0500

    Renamed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,4 +8,4 @@ sudo cp interfaces /etc/network/interfaces
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant.conf
 
 # set up udev rules
-sudo cp 10-udev.rules /etc/udev/rules.d/
+sudo cp udev.rules /etc/udev/rules.d/20-quaddard.rules
