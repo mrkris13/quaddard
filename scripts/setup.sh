5a1478eb7fb5c8dfb3fa9b74ac61e697cdb3574a
Kris Frey <kfrey@mit.edu>
2016-02-01 14:02:25 -0500

    Added udev rules for px4

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,6 @@ echo 'source $HOME/quaddard/scripts/setup.bash' >> $HOME/.bashrc
 # set up networking
 sudo cp interfaces /etc/network/interfaces
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant.conf
+
+# set up udev rules
+sudo cp 10-udev.rules /etc/udev/rules.d/
